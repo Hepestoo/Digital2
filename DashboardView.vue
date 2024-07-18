a09519335077a1bd85da2bf86585a6ca251f320e
Hepestoo <teckalan2@gmail.com>
2024-07-17 21:15:39 -0500

    volvi a hacer el dash, un poco inventario y movi un css el css del header

---
+++ DashboardView.vue
@@ -0,0 +1,378 @@
+<template>
+  <HeaderView/>
+  <aside class="sidebar">
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/dashboard" class="active">
+          <i class="fa-solid fa-chart-simple"></i> Dashboard
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/menus">
+          <i class="fa-solid fa-envelope"></i> Menu
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/visualizar">
+          <i class="fa-solid fa-table-cells-large"></i> Visualizar
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/empleados">
+          <i class="fa-solid fa-person"></i> Empleados
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/pagos">
+          <i class="fa-regular fa-credit-card"></i> Metodos de pago
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/proveedores">
+          <i class="fa-solid fa-user-group"></i> Proveedores
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/inventario">
+          <i class="fa-solid fa-file-invoice-dollar"></i> Inventario
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+  </aside>
+<!-- ! Termina el SIDEBAR -->
+  <div class="container">
+    <h1>Dashboard</h1>
+
+    <div class="tarjetas">
+      <div class="tarjeta1">
+        <h3>Ingresos Totales</h3>
+        <p>3,291,922</p>
+        <img src="" alt="">
+        <button>View Detail →</button>
+      </div>
+      <div class="tarjeta2">
+        <h3>Pedidos de hoy</h3>
+        <p>290</p>
+        <img src="" alt="">
+        <button>View Detail →</button>
+      </div>
+      <div class="tarjeta3">
+        <h3>Pedidos Pendientes</h3>
+        <p>10</p>
+        <img src="" alt="">
+        <button>View Detail →</button>
+      </div>
+      <div class="tarjeta4">
+        <h3>Pedidos Entregados</h3>
+        <p>400</p>
+        <img src="" alt="">
+        <button>View Detail →</button>
+      </div>
+    </div>
+
+    <div class="seccion-media">
+
+      <div class="izquierda">
+        <div class="categorias">
+          <h3>Categorias</h3>
+            <div class="tarjetas-container">
+              <div class="entradas">
+                <h3>Entradas</h3>
+                <button>Ver</button>
+              </div>
+              <div class="sopas">
+                <h3>Sopas</h3>
+                <button>Ver</button>
+              </div>
+              <div class="bebidas">
+                <h3>Bebidas</h3>
+                <button>Ver</button>
+              </div>
+              <div class="postres">
+                <h3>Postres</h3>
+                <button>Ver</button>
+              </div>
+            </div>
+        </div>
+        <div class="redes">
+          <h3>Redes Sociales</h3>
+          <i class="fa-brands fa-youtube" style="color: #787d87;"></i>
+          <i class="fa-brands fa-tiktok" style="color: #787d87;"></i>
+          <i class="fa-brands fa-telegram" style="color: #787d87;"></i>
+          <i class="fa-brands fa-linkedin" style="color: #787d87;"></i>
+          <i class="fa-brands fa-instagram" style="color: #787d87;"></i>
+          <i class="fa-brands fa-x-twitter" style="color: #787d87;"></i>
+        </div>
+      </div>
+
+      <div class="derecha">
+        <div class="anuncio">
+          <h2>Digital Waiter <br>es tu mejor acompanante <br>a la hora de <br>administar tu restaurante...</h2>
+        </div>
+        <div class="empleados">
+          <h3>Empleados</h3>
+          <div class="empleado1">
+              <i class="fa-solid fa-user"></i>
+              <div class="empleado-info">
+                <h5>Juan Perez</h5>
+                <p>Mesero</p>
+              </div>
+          </div>
+          <div class="empleado2">
+            <i class="fa-solid fa-user"></i>
+            <div class="empleado-info">
+                <h5>Juan Perez</h5>
+                <p>Mesero</p>
+              </div>
+          </div>
+          <div class="empleado3">
+            <i class="fa-solid fa-user"></i>
+            <div class="empleado-info">
+                <h5>Juan Perez</h5>
+                <p>Mesero</p>
+              </div>
+          </div>
+          <div class="empleado4">
+            <i class="fa-solid fa-user"></i>
+            <div class="empleado-info">
+                <h5>Juan Perez</h5>
+                <p>Mesero</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import HeaderView from '@/components/header/HeaderView.vue';
+  export default {
+  name: 'App',
+  components: {
+    HeaderView
+  }
+}
+</script>
+
+<style scoped>
+body{
+  padding: 0;
+  margin: 0
+}
+.container{
+  width: 100%;
+}
+.container h1{
+  color: #6C6C6C;
+  margin-left: 30px; 
+  margin-top: 100px
+}
+.tarjetas{
+  background-color: #2E2C2C;
+  display: flex;
+  width: 95%;
+  height: 20%;
+  margin: auto;
+  margin-top: 10px;
+  border-radius: 10px;
+}
+.tarjeta1, .tarjeta2, .tarjeta3, .tarjeta4{
+  margin: auto;
+  width: 250px;
+  height: 120px;
+}
+.tarjeta1{
+  background-color: #348FE2;
+}
+.tarjeta2{
+  background-color: #49B6D6;
+}
+.tarjeta3{
+  background-color: #FF5B57;
+}
+.tarjeta4{
+  background-color: #F59C1A;
+}
+.tarjeta1 h3, .tarjeta2 h3, .tarjeta3 h3, .tarjeta4 h3{
+  color: #FFFFFF;
+  margin-left: 20px;
+  margin-top: 20px;
+  font-weight: 400;
+  font-size: 15px;
+}
+.tarjeta1 p, .tarjeta2 p, .tarjeta3 p, .tarjeta4 p{
+  color: #FFFFFF;
+  font-weight: 400;
+  margin-left: 20px;
+  margin-top: 10px;
+  font-size: 30px;
+}
+.tarjeta1 button, .tarjeta2 button, .tarjeta3 button, .tarjeta4 button{
+  height: 30px;
+  border: none;
+  margin-top: 8px;
+  color: #8C9094;
+  text-align: right;
+  cursor: pointer;
+  padding-right: 10px;
+}
+.tarjeta1 button{
+  background-color: #15395A;
+}
+.tarjeta2 button{
+  background-color: #2C6D80;
+} 
+.tarjeta3 button {
+  background-color: #993734;
+}
+.tarjeta4 button{
+  background-color: #623E0A;
+}
+.tarjeta1 button, .tarjeta2 button, .tarjeta3 button, .tarjeta4 button{
+  width: 100%;
+}
+
+.seccion-media{
+  display: flex;
+  height: 61%;
+  width: 95%; 
+  margin: auto
+}
+.izquierda{
+  width: 75%;
+}
+.categorias{
+  background-color: #2A2A2A;
+  border-radius: 10px;
+  width: 96%;
+  height: 55%;
+  margin: auto;
+  margin-top: 20px;
+}
+.categorias h3{
+  color: #979797;
+  padding: 5px 10px;
+}
+.tarjetas-container{
+  display: flex;
+}
+.entradas, .sopas, .bebidas, .postres{
+  margin: auto;
+  height: 220px;
+  width: 190px;
+  border-radius: 10px;
+
+}
+.entradas{
+  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url("../../assets/img/ceviche.png") no-repeat center / cover;
+}
+.sopas{
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../../assets/img/sopa.png") no-repeat center / cover;
+}
+
+.bebidas{
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../../assets/img/trago.png") no-repeat center / cover;
+}
+
+.postres{
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../../assets/img/brownie.png") no-repeat center / cover;
+}
+.entradas h3, .sopas h3, .bebidas h3, .postres h3{
+  color: #CCCCCC;
+  font-size: 25px;
+  font-weight: 400;
+  margin-top: 15px;
+  margin-left: 15px;
+}
+.entradas button, .sopas button, .bebidas button, .postres button{
+  color: #FFFFFF;
+  font-size: 20px;
+  background-color: #0085FF;
+  border: none;
+  width: 60%;
+  height: 30px;
+  margin-top: 100px;
+  margin-left: 40px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+.redes{
+  width: 95%;
+  margin: auto;
+  margin-top: 20px;
+  height: 30%;
+  border: 4px solid #BDBDBD;
+  border-radius: 20px;
+}
+.redes h3{
+  color: #979797;
+  padding: 5px 10px;
+}
+.redes i{
+  font-size: 60px;
+  margin: auto;
+  margin-left: 20px;
+  margin-top: 10px;
+  padding: 0 30px;
+  cursor: pointer;
+}
+.derecha{
+  width: 25%;
+}
+.anuncio{
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../../assets/img/anuncio.png") no-repeat center / cover;
+  width: 300px;
+  height: 200px;
+  border-radius: 20px;
+  margin: auto;
+  margin-top: 20px;
+}
+.anuncio h2{
+  color: #A7A7A7;
+  padding: 20px;
+}
+.empleados{
+  background-color: #323232;
+  margin-top: 15px;
+  height: 48%;
+  border-radius: 10px;
+}
+.empleados h3{
+  color: #979797;
+  padding: 10px 10px;
+}
+
+.empleado1, .empleado2, .empleado3, .empleado4{
+  background-color: #D9D9D9;
+  width: 88%;
+  height: 40px;
+  margin: auto;
+  margin-bottom: 5px;
+  border: 1px solid black;
+  font-size: 20px;
+  display: flex;
+}
+.empleado1 i, .empleado2 i, .empleado3 i, .empleado4 i {
+  margin: auto;
+  margin-left: 20px;
+}
+.empleado-info{
+  display: flex;
+}
+.empleado-info h5 {
+  margin: auto;
+  margin-right: 40px;
+  font-size: 16px;
+}
+
+.empleado-info p {
+  margin: auto;
+  font-size: 13px;
+  margin-right: 15px;
+  color: #666;
+  font-weight: bold
+}
+</style>
---
+++ VisualizarView.vue
@@ -0,0 +1,344 @@
+<template>
+    <HeaderView/>
+        <aside class="sidebar">
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/dashboard" class="active">
+                    <i class="fa-solid fa-chart-simple"></i> Dashboard
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/menus">
+                    <i class="fa-solid fa-envelope"></i> Menu
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/visualizar">
+                <i class="fa-solid fa-table-cells-large"></i> Visualizar
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/empleados">
+                    <i class="fa-solid fa-person"></i> Empleados
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/pagos">
+                    <i class="fa-regular fa-credit-card"></i> Metodos de pago
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/proveedores">
+                    <i class="fa-solid fa-user-group"></i> Proveedores
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/inventario">
+                    <i class="fa-solid fa-file-invoice-dollar"></i> Inventario
+                </router-link>
+            </li>
+        </ul>
+    </nav>
+</aside>
+<!-- ! Termina el SIDEBAR -->
+    <div class="container">
+        <h1>Visualización de menú</h1>
+        <div class="content">
+
+            <div class="izquierda">
+                <h1>MENU</h1>
+                <div class="bloque1">
+
+                <h3>Entrantes</h3>
+                <h4>Entradas frias</h4>
+                <div class="entrada-seccion">
+                    <div class="entrada-texto">
+                        <p>Alitas BBQ <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                    </div>
+                    <img src="../../assets/img/ceviche.png" alt="">
+                </div>
+
+                <h4>Entradas Calientes</h4>
+                <div class="entrada-seccion">
+                    <div class="entrada-texto">
+                        <p>Alitas BBQ <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                    </div>
+                    <!-- Puedes agregar una imagen aquí si lo deseas -->
+                </div>
+
+                <h4>Aperitivos</h4>
+                <div class="entrada-seccion">
+                    <div class="entrada-texto">
+                        <p>Alitas BBQ <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                    </div>
+                    <img src="../../assets/img/hamburguer.png" alt="">
+                </div>
+
+                <h4>Snacks</h4>
+                <div class="entrada-seccion">
+                    <div class="entrada-texto">
+                        <p>Alitas BBQ <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                    </div>
+                    <!-- Puedes agregar una imagen aquí si lo deseas -->
+                </div>
+            </div>
+            </div>
+
+
+            <div class="medio">
+                <div class="bloque2">
+                    <h3>Bebidas</h3>
+                    <h4>Sin alcohol</h4>
+                    <div class="bebida-seccion">
+                        <div class="bebida-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                            <p>Costillas <span>15.00</span></p>
+                        </div>
+                        <img src="../../assets/img/trago.png" alt="">
+                    </div>
+
+                    <h4>Con alcohol</h4>
+                    <div class="bebida-seccion">
+                        <div class="bebida-texto">
+                        <p>Alitas BBQ <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                        <p>Costillas <span>15.00</span></p>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="bloque3">
+                    <h3>Postres</h3>
+                    <h4>Frios</h4>
+                    <div class="postre-seccion">
+                        <div class="postre-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                            <p>Alitas BBQ <span>15.00</span></p>
+                        </div>
+                    </div>
+
+                    <h4>Calientes</h4>
+                    <div class="postre-seccion">
+                        <div class="postre-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                            <p>Alitas BBQ <span>15.00</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="derecha">
+                <div class="bloque4">
+                    <h3>Sopas</h3>
+                    <h4>Mariscos</h4>
+                    <div class="sopa-seccion">
+                        <div class="sopa-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                            <p>Alitas BBQ <span>15.00</span></p>
+                        </div>
+                    </div>
+                    <h4>Cremas</h4>
+                    <div class="sopa-seccion">
+                        <div class="sopa-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                            <p>Alitas BBQ <span>15.00</span></p>
+                        </div>
+                    </div>
+                    <h4>Caldos</h4>
+                    <div class="sopa-seccion">
+                        <div class="sopa-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bloque5">
+                    <h3>Helados</h3>
+                    <h4>Naturales</h4>
+                    <div class="helado-seccion">
+                        <div class="helado-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                            <p>Alitas BBQ <span>15.00</span></p>
+                        </div>
+                    </div>
+                    <h4>Especiales</h4>
+                    <div class="helado-seccion">
+                        <div class="helado-texto">
+                            <p>Alitas BBQ <span>15.00</span></p>
+                            <p>Alitas BBQ <span>15.00</span></p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+import HeaderView from '@/components/header/HeaderView.vue';
+
+export default {
+    name: 'MenuListView',
+    components: {
+        HeaderView
+    }
+};
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Peralta&display=swap');
+
+.container {
+    width: 100%;
+}
+
+.container h1 {
+    color: #6C6C6C;
+    margin-top: 80px;
+    padding-left: 50px;
+}
+
+.content {
+    display: flex;
+    justify-content: space-between;
+    margin-left: 30px;
+}
+
+.izquierda, .medio, .derecha {
+    height: 600px; /* Ajusta esta altura según tus necesidades */
+    width: 700px;
+    box-sizing: border-box;
+    margin-left: 10px;
+}
+
+.izquierda {
+    background-color: #353535;
+}
+.izquierda h1{
+    color: #FFFBF0;
+    font-size: 120px;
+    padding: 0 0 0 10px;
+    margin: 0;
+}
+
+.medio {
+    background-color: #353535;
+}
+
+.derecha {
+    background-color: #353535;
+}
+
+.bloque1{
+    height: 70%;
+}
+.bloque2{
+    
+}
+.entrada-seccion {
+    display: flex;
+    height: 70px;
+    margin-bottom: -30px;
+    justify-content: space-between;
+}
+.bebida-seccion {
+    display: flex;
+    height: 70px;
+    margin-bottom: -30px;
+
+    justify-content: space-between;
+}
+.postre-seccion {
+    display: flex;
+    justify-content: space-between;
+}
+.sopa-seccion {
+    display: flex;
+    justify-content: space-between;
+}
+.helado-seccion {
+    display: flex;
+    justify-content: space-between;
+}
+.bloque1 h3{
+    text-align: center;
+    color: #FFFBF0;
+    font-weight: 300;
+    background-color: red;
+    width: 50%;
+    height: 25px;
+    margin: auto;
+    margin-bottom: -25px;
+
+}
+.bloque2 h3,.bloque3 h3,.bloque4 h3,.bloque5 h3{
+    color: #FFFBF0;
+    font-weight: 300;
+    margin: 0;
+    margin-top: 60px;
+    margin-left: 20px;
+    margin-bottom: -25px;
+    background-color: red;
+    width: 50%;
+    text-align: center;
+    font-size: 20px;
+}
+.bloque1 img{
+    border-radius: 100%;
+    width: 70px;
+    height: 70px;
+    border: 5px solid #FFFFFF;
+}
+.bloque2 img{
+    border-radius: 100%;
+    width: 70px;
+    height: 70px;
+    border: 5px solid #FFFFFF;
+}
+.bloque2, .bloque3, .bloque4, .bloque5{
+    height: 50%;
+    margin-top: -38px;
+}
+.bloque1 h4, .bloque2 h4,.bloque3 h4,.bloque4 h4,.bloque5 h4{
+    margin-top: 35px;
+    margin-left: 20px;
+    font-family: "Peralta", sans-serif;
+}
+.bloque1 p, .bloque2 p,.bloque3 p,.bloque4 p,.bloque5 p{
+    color: #FFFBF0;
+    margin-left: 20px;
+    margin-top: 10px;
+}
+.bloque1 span, .bloque2 span,.bloque3 span,.bloque4 span,.bloque5 span{
+    color: #FFFBF0;
+    font-weight: bold;
+}
+.bloque1 h4{
+    color: #FFBB00;
+    
+}
+.bloque2 h4{
+    color: #FA00FF;
+    
+}
+.bloque3 h4{
+    color: #55D25A;
+    
+}
+.bloque4 h4{
+    color: #0094FF;
+    
+}
+.bloque5 h4{
+    color: #FFF500;
+    
+}
+</style>
